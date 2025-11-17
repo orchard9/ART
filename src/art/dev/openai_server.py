c50f1d2b35cf4dfa5083ac1a7fffffe1b8643874
Vivek Kalyan <hello@vivekkalyan.com>
2025-11-17 23:14:57 +0800

    fix: Remove unused variable in vllm=0.10.2

--- src/art/dev/openai_server.py
+++ src/art/dev/openai_server.py
@@ -29,7 +29,7 @@ def get_openai_server_config(
     server_args.update(config.get("server_args", {}))
     engine_args = EngineArgs(
         model=base_model,
-        num_scheduler_steps=16 if lora_path else 1,
+        # num_scheduler_steps=16 if lora_path else 1,
         served_model_name=base_model if lora_path else model_name,
         disable_log_requests=True,
         generation_config="vllm",
